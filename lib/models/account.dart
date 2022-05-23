8b808b1018cb473ea93765ad90adee48a82a1a77
Andrew Wood <andrew.wood@ualberta.ca>
2022-05-23 14:35:54 -0600

    Rounding account response

--- lib/models/account.dart
+++ lib/models/account.dart
@@ -12,14 +12,22 @@ class Account {
   });
 
   factory Account.fromJson(Map<String, dynamic> json) {
+    // Takes a string-type number and returns it with two decimal places
+    String roundNumber(String number) {
+      double numAsDouble = double.parse(number);
+      String roundedNumber = numAsDouble.toStringAsFixed(2);
+      return roundedNumber;
+    }
+
     // Creates a string for the current date and time
     DateTime now = DateTime.now();
-    String convertedDateTime = "${now.year.toString()}-${now.month.toString().padLeft(2,'0')}-${now.day.toString().padLeft(2,'0')} ${now.hour.toString().padLeft(2,'0')}:${now.minute.toString().padLeft(2,'0')}";
-    
+    String convertedDateTime =
+        "${now.year.toString()}-${now.month.toString().padLeft(2, '0')}-${now.day.toString().padLeft(2, '0')} ${now.hour.toString().padLeft(2, '0')}:${now.minute.toString().padLeft(2, '0')}:${now.second.toString().padLeft(2, '0')}";
+
     return Account(
-      buyingPower: json['buying_power'],
-      cash: json['cash'],
-      equity: json['equity'],
+      buyingPower: roundNumber(json['buying_power']),
+      cash: roundNumber(json['cash']),
+      equity: roundNumber(json['equity']),
       date: convertedDateTime,
     );
   }
